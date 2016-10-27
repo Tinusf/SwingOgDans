71f9fe708ca02ee5de0848c075201760c0062a9b
Sigve Sjømæling Nordgaard <sigvesn@dhcp-10-22-155-62.wlan.ntnu.no>
2016-10-27 11:01:58 +0200

    Oppdatert II

--- Website/scripts/textClick.js
+++ Website/scripts/textClick.js
@@ -1,20 +1,12 @@
 function textClick(a,b) {
  	var a = document.getElementById(a);
  	var b = document.getElementById(b);
-    var w = parseInt(window.innerWidth);
-    console.log(w < 940);
  	if(a.style.display == 'none' || a.style.display == ''){
     	a.style.display = 'block';
     	b.innerHTML = '−';
- 
     }
  	else{
     	a.style.display = 'none';
     	b.innerHTML = '+';
-    	
-    
     }
 }
-
-
-
