4a8df95f04371e344b7d1282a8f571519b17899f
Tinus Flagstad <tinus@flagstad.as>
2016-11-01 14:09:26 +0100

    Endret musikk og javascripten til noe mer fancy som funker.

--- Website/scripts/musicArrow.js
+++ Website/scripts/musicArrow.js
@@ -1,26 +1,57 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+const musicNext = document.getElementById("musicNext");
+const musicPrev = document.getElementById("musicPrev");
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+const musiciFrame1 = document.getElementById("musiciFrame1");
+const musiciFrame2 = document.getElementById("musiciFrame2");
+const musiciFrame3 = document.getElementById("musiciFrame3");
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+const musicInfo1 = document.getElementById("musicInfo1");
+const musicInfo2 = document.getElementById("musicInfo2");
+const musicInfo3 = document.getElementById("musicInfo3");
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+let musicArray = [];
+let musicInfoArray = [];
+let index = [0, 1, 2];
+
+
+musicArray.push("https://embed.spotify.com/?uri=spotify:user:andreskr:playlist:6dTjFmqFb2KHZy9f6VPwce");
+musicArray.push("https://embed.spotify.com/?uri=spotify:user:andreskr:playlist:6Nh1LYRncSJRSMhgh6YlBP");
+musicArray.push("https://embed.spotify.com/?uri=spotify:user:andreskr:playlist:1QvfyT3hSLYXMHw1u1Xo5F");
+musicArray.push("https://embed.spotify.com/?uri=spotify:user:andreskr:playlist:7KQNQyDeMzh9IApGLInOM4");
+musicArray.push("https://embed.spotify.com/?uri=spotify:user:andreskr:playlist:4j46yi8Ah1NUPgtdS9DXY8");
+
+musicInfoArray.push("Swing - 110");
+musicInfoArray.push("Swing 111-120");
+musicInfoArray.push("Swing 121-130");
+musicInfoArray.push("Swing 131-140");
+musicInfoArray.push("Swing 141-150");
+
+musicPrev.addEventListener("click", function() {
+	for (let x = 0; x< 3; x++) {
+		index[x]--;
+		if (index[x] < 0) {
+		index[x] = musicArray.length-1;
+		}
+	}
+	changePandArray();
+});
+
+
+musicNext.addEventListener("click", function() {
+	for (let x = 0; x< 3; x++) {
+		index[x]++;
+		if (index[x] > musicArray.length - 1) {
+		index[x] = 0;
+		}
+	}
+	changePandArray();
+});
+
+function changePandArray() {
+	musiciFrame1.src = musicArray[index[0]];
+	musicInfo1.innerHTML = musicInfoArray[index[0]];
+	musiciFrame2.src = musicArray[index[1]];
+	musicInfo2.innerHTML = musicInfoArray[index[1]];
+	musiciFrame3.src = musicArray[index[2]];
+	musicInfo3.innerHTML = musicInfoArray[index[2]];
 }
