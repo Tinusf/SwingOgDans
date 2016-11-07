665ad17ddd99b7956fece46d5dd9387063f7ad84
Tinus Flagstad <tinus@flagstad.as>
2016-11-07 19:29:50 +0100

    la til kommentarer

--- Website/scripts/navbarMenu.js
+++ Website/scripts/navbarMenu.js
@@ -1,4 +1,11 @@
+/*
+FILE NAME: navbarMenu.js 
+WRITTEN BY: Tinus Flagstad 
+WHEN: September 2016
+PURPOSE: Denne funksjonen bytter bare klassen til navbarID så jeg vet om den skal være responsiv eller ikke. Denne bytter jo annenhver gang. 
+*/
 function navBarMenu() {
+	
 	var navbarID = document.getElementById("navbarID");
 	if (navbarID.className === "navbarClass") {
 		navbarID.className += " responsive";
