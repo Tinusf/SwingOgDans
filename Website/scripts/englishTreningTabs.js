16d7a0b7b59fd1401c1281d1b4cc73835d4485dc
Ebba <Ebba>
2016-11-09 13:00:01 +0100

    la til kommentarer

--- Website/scripts/englishTreningTabs.js
+++ Website/scripts/englishTreningTabs.js
@@ -1,14 +1,24 @@
+/*englishTreningTabs.js
+WRITTEN BY: Kode originalt skrevet av Sigve, men tilpasset til "english" nettsiden av Ebba Fingarsen
+WHEN: September 2016
+PURPOSE: 
+*/
+
+/* Henter inn treningslistene med id og lagrer de som konstanter. */
 const treningList1 = document.getElementById("treningList1");
 const treningList2 = document.getElementById("treningList2");
 const treningList3 = document.getElementById("treningList3");
 const treningList4 = document.getElementById("treningList4");
 
+/* Henter inn tid, kurs og sted med id og lagrer de som konstanter */
 const treningsTid = document.getElementById("treningsTid");
 const treningsKurs = document.getElementById("treningsKurs");
 const treningsSted = document.getElementById("treningsSted");
 
+/* Henter inn teningsteksten med id og lagrer de som konstanter */
 const treningsTekst = document.getElementById("treningsTekst");
 
+/* Legger til eventlistner på treningsList1. Ved klikk skal tid, kurs, sted og treningstekst endres. Gjør dette ved bruk av .innerHTML */
 treningList1.addEventListener("click", function() { 
 	treningsTid.innerHTML="20:15 – 22:00";
 	treningsKurs.innerHTML = "Swing (Advanced)";
@@ -16,6 +26,7 @@ treningList1.addEventListener("click", function() {
 	treningsTekst.innerHTML = "This course is intended for those who have danced swing for at least one year (two semesters) with us. The course focuses on advanced rides and technique, with some other spices for example: Boy with two girls and jump / lift. If you are unsure which level you should be on, or wonder something else: do not hesitate to contact one of the instructors. We recommend having been through at least two semesters with beginners course before joining the secondary party, this is important to maintain the level of this course for all participants, since we unfortunately only have courses on two levels.";
 });
 
+/* Legger til eventlistner på treningsList2. Ved klikk skal tid, kurs, sted og treningstekst endres. Gjør dette ved bruk av .innerHTML */
 treningList2.addEventListener("click", function() { 
 	treningsTid.innerHTML="18:30 – 20:00";
 	treningsKurs.innerHTML = "Swing (Beginner)";
@@ -23,6 +34,7 @@ treningList2.addEventListener("click", function() {
 	treningsTekst.innerHTML ="The beginners course in swing suitable for those who have never danced before and those who have danced less than 1-2 years, but is also very educational for everyone else. We start with 1.5 hours of instruction, and then there free-dance the rest of the evening. It instructed 4-speed swing (the most common swing-type in Norway). We keep quite basic intro courses the first three times in each semester, for these are autumn and spring masters different, so that you learn new things throughout the year. That is if you can try their hand at the upper portion if one feels in control after two semesters. ";
 });
 
+/* Legger til eventlistner på treningsList3. Ved klikk skal tid, kurs, sted og treningstekst endres. Gjør dette ved bruk av .innerHTML */
 treningList3.addEventListener("click", function() { 
 	treningsTid.innerHTML="20.00 – 22.00";
 	treningsKurs.innerHTML = "Folk dance (Everyone)";
@@ -30,6 +42,7 @@ treningList3.addEventListener("click", function() {
 	treningsTekst.innerHTML = "Would you like to learn Norwegian folk dance? Or have you danced before, but miss someone to dance with? All are welome, regardless of level. We focus on reinlender, waltz, pols and Halling, but during the semester, we can also come by springar, polka, tango and much more fun! We emphasize to teach it to lead and to follow, which is essential in all couples dance. Folk dance trainings can therefore also be a useful supplement for those involved with other dances. We travel too happy together on the dance evenings in the Trondheim area and organizes joint trips to Rørosmartnan and Hilmarfestivalen..Kalender";
 });
 
+/* Legger til eventlistner på treningsList4. Ved klikk skal tid, kurs, sted og treningstekst endres. Gjør dette ved bruk av .innerHTML */
 treningList4.addEventListener("click", function() { 
 	treningsTid.innerHTML="20.30 – 22.00";
 	treningsKurs.innerHTML = "Free-dance Swing (Everyone)";
--- Website/scripts/musicArrow.js
+++ Website/scripts/musicArrow.js
@@ -1,19 +1,29 @@
+/*musicArrow.js
+WRITTEN BY: Ebba Fingarsen
+WHEN: September 2016
+PURPOSE: Få "rulleringen" av busikkbokser til å fungere ved både bruk av "musicPrev" og "musicNext".
+*/
+
+/* henter inn knapper med id og lagrer det i konstanter */
 const musicNext = document.getElementById("musicNext");
 const musicPrev = document.getElementById("musicPrev");
 
+/* henter inn musikkrammene(iframe) med id og lagrer det i konstanter */
 const musiciFrame1 = document.getElementById("musiciFrame1");
 const musiciFrame2 = document.getElementById("musiciFrame2");
 const musiciFrame3 = document.getElementById("musiciFrame3");
 
+/* Henter inn musikkinfo(info om tempo) med id og lagrer det i konstanter */
 const musicInfo1 = document.getElementById("musicInfo1");
 const musicInfo2 = document.getElementById("musicInfo2");
 const musicInfo3 = document.getElementById("musicInfo3");
 
+/* Lager en tom liste for musikkbokser og en tom liste for musikkinfo.*/
 let musicArray = [];
 let musicInfoArray = [];
 let index = [0, 1, 2];
 
-
+/* Bruker .push til å legge til linken til alle spotify iframe'ene i "musicArray" slik at denne listen kan rulleres på. */
 musicArray.push("https://embed.spotify.com/?uri=spotify:user:andreskr:playlist:6dTjFmqFb2KHZy9f6VPwce");
 musicArray.push("https://embed.spotify.com/?uri=spotify:user:andreskr:playlist:6Nh1LYRncSJRSMhgh6YlBP");
 musicArray.push("https://embed.spotify.com/?uri=spotify:user:andreskr:playlist:1QvfyT3hSLYXMHw1u1Xo5F");
@@ -26,6 +36,7 @@ musicArray.push("https://embed.spotify.com/?uri=spotify:user:andreskr:playlist:1
 musicArray.push("https://embed.spotify.com/?uri=spotify:user:andreskr:playlist:4O4SRDt3DIcrlm40ddEbxj");
 musicArray.push("https://embed.spotify.com/?uri=spotify:user:andreskr:playlist:5MJCuU8WpPqAiD0ujhuUdB");
 
+/* Bruker .push til å legge til info om tempo til hver spilleliste i "musicInfoArray" slik at denne listen kan rulleres på  på lik linje med "musicArray". */
 musicInfoArray.push("Swing < 110");
 musicInfoArray.push("Swing 111-120");
 musicInfoArray.push("Swing 121-130");
@@ -38,6 +49,7 @@ musicInfoArray.push("Swing 181-190");
 musicInfoArray.push("Swing 191-200");
 musicInfoArray.push("Swing > 200");
 
+/* Eventlistener på "musicPrev" knappen med en funksjon som senker index'en, om index'en er mindre enn 0 bli index lik lengden til "musicArray" -1 (looper listen)*/
 musicPrev.addEventListener("click", function() {
 	for (let x = 0; x< 3; x++) {
 		index[x]--;
@@ -48,7 +60,7 @@ musicPrev.addEventListener("click", function() {
 	changePandArray();
 });
 
-
+/* Eventlistener på "musicNext" knappen med en funksjon som øker index'en, om index'en er større enn lengden til "musicArray"-1 bli index lik 0 (looper listen)*/
 musicNext.addEventListener("click", function() {
 	for (let x = 0; x< 3; x++) {
 		index[x]++;
