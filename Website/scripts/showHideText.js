513415db846c2070df384ca4c5d20af343d60714
Sigve Sjømæling Nordgaard <sigvesn@Sigves-MBP-2.lan>
2016-11-12 20:19:54 +0100

    Oppdatert kommentarer

--- Website/scripts/showHideText.js
+++ Website/scripts/showHideText.js
@@ -1,12 +1,21 @@
-function textClick(a,b) {
-	var a = document.getElementById(a);
-	var b = document.getElementById(b);
-	if(a.style.display == 'none' || a.style.display == ''){
-		a.style.display = 'block';
-		b.innerHTML = '−';
+/*
+FILE NAME: showHideText.js
+WRITTEN BY: Sigve S. Nordgaard
+WHEN: Oktober 2016
+PURPOSE: Shows and hides text when the text is clicked, and changes the symbol between '+' and '-' 
+*/
+function textClick(text,symbol) {
+	var text = document.getElementById(text);
+	var symbol = document.getElementById(symbol);
+	/* If display is set to none(text hidden) show the text, else the text is shown(as block) and we want to hide it.
+	Because javascrips only sets and gets style from the html and style is originally set in css, we have to check if 
+	its the first time we change the style with javascript by checking for empty string in style.display. */
+	if(text.style.display == 'none' || text.style.display == ''){
+		text.style.display = 'block';
+		symbol.innerHTML = '−';
 	}
 	else{
-		a.style.display = 'none';
-		b.innerHTML = '+';
+		text.style.display = 'none';
+		symbol.innerHTML = '+';
 	}
 }
